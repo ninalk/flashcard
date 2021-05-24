9396b2df1ed92bd20c2c97b0eab2f4ce2e425722
Nina Kim <neenerskim@gmail.com>
2021-05-23 20:58:57 -0700

    set up user login

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -2,17 +2,80 @@ import React, { useState } from 'react';
 import './LoginPage.css';
 import ErrorMessage from '../../components/ErrorMessage/ErrorMessage'
 import userService from '../../utils/userService';
+import { useHistory, Link } from 'react-router-dom';
+import { Button, Form, Grid, Header, Message, Segment, Image } from 'semantic-ui-react';
 
 
 export default function LoginPage(props){
-    
+    const history = useHistory();
+    const [error, setError] = useState('');
+    const [state, setState] = useState({
+        username: '',
+        password: ''
+    });
+
+    function handleChange(e){
+        setState({
+            ...state,
+            [e.target.name]: e.target.value
+        });
+    }
+
+    async function handleSubmit(e){
+        e.preventDefault();
+
+        try {
+            await userService.login(state);
+            props.handleSignUpOrLogin();
+            history.push('/');
+        } catch(err){
+            setError(err.message);
+        }
+    }
 
     return (
-      
-          
-             <h1>setup signup PAGE</h1>
-    
-      
+        <>
+            <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
+                <Grid.Column style={{ maxWidth: 450 }} >
+                    <Form  autoComplete="off" onSubmit={handleSubmit} >                        
+                        <Header as='h2' textAlign='center' className="signup-logo">
+                            <span className='logo-text'> Flash Card App</span>
+                        </Header>
+                        <Segment stacked>
+                            <Form.Input
+                            type="username"                           
+                            name="username"
+                            placeholder="username"
+                            value={state.username}
+                            onChange={handleChange}
+                            required
+                            />
+                            <Form.Input
+                            name="password"
+                            type="password"
+                            placeholder="password"
+                            value={state.password}
+                            onChange={handleChange}
+                            required
+                            />
+                        <Button
+                            color='teal'
+                            fluid size='large'
+                            type="submit"
+                            className="btn"
+                            // disabled={invalidForm}
+                        >
+                            Login
+                        </Button>
+                        <Message>
+                            New to us? <Link to='/signup'>&nbsp;Sign Up</Link>
+                        </Message>
+                        </Segment>
+                    </Form>
+                </Grid.Column>
+                {error ? <ErrorMessage error={error} /> : null}
+            </Grid>
+      </>
       );
 }
 
