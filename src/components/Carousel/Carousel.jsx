dbc30e3e7d90dddb563a61c80774e3829ff1ce00
Nina Kim <neenerskim@gmail.com>
2021-08-05 16:31:15 -0700

    add comments to carousel component

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -3,6 +3,7 @@ import './Carousel.css';
 import ShuffleButton from '../../components/ShuffleButton/ShuffleButton';
 import { useSwipeable } from 'react-swipeable';
 
+// CarouselItem renders the item itself
 export const CarouselItem = ({children, width}) => {
     return (
         <div className='carousel-item' style={{ width: width }}>
@@ -11,9 +12,11 @@ export const CarouselItem = ({children, width}) => {
     )
 }
 
+// Carousel is the main controller to control items
 export default function Carousel({ children, removeCard, updateCard, shuffleCards, cards }) {
     const [activeIndex, setActiveIndex] = useState(0);
 
+    //  method to update new active index
     const updateIndex = (newIndex) => {
         if (newIndex < 0) {
             newIndex = React.Children.count(children) - 1;
@@ -24,6 +27,7 @@ export default function Carousel({ children, removeCard, updateCard, shuffleCard
         setActiveIndex(newIndex);
     }
 
+    // use react-swipeable package to swipe on mobile devices
     const handlers = useSwipeable({
         onSwipedLeft: () => updateIndex(activeIndex + 1),
         onSwipedRight: () => updateIndex(activeIndex - 1)
@@ -31,7 +35,10 @@ export default function Carousel({ children, removeCard, updateCard, shuffleCard
     
     return (
         <div {...handlers} className='carousel'>
+            {/* use transform: translateX(-100%) to make 1 active item visible at 100% width */}
             <div className='inner' style={{ transform: `translateX(-${activeIndex * 100}%)` }}>
+                {/* Iterate through all children and adjust width property to show only 1 item */}
+                {/* cloneElement results in the element having the original elements props, key, and ref */}
                 {React.Children.map(children, (child, index) => {
                     return React.cloneElement(child, { width: '100%' });
                 })}
